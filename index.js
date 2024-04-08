a27edf6470d35fac1c77eb9915ca891eb073084c
Vinh <vhlemail12@gmail.com>
2024-04-08 13:13:22 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.querySelector(`main`)
+main.remove()
+
+const newHeader = document.createElement(`h1`)
+    newHeader.id = "victory"
+    newHeader.innerHTML = "Vinh is the champion"
